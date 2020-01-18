7ed9e3d2333dc20fcfaa91ce3c283298eafea4ef
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-01-18 15:47:20 -0300

    Adding database access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -3,17 +3,22 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Livros"));
-		list.add(new Department(2, "Eletrônicos"));
-		list.add(new Department(3, "Computadores"));
-		list.add(new Department(4, "Filmes"));
-		list.add(new Department(5, "Vestuário"));
-		return list;
+		return dao.findAll();
+		//List<Department> list = new ArrayList<>();
+		//list.add(new Department(1, "Livros"));
+		//list.add(new Department(2, "Eletrônicos"));
+		//list.add(new Department(3, "Computadores"));
+		//list.add(new Department(4, "Filmes"));
+		//list.add(new Department(5, "Vestuário"));
+		//return list;
 	}
 }
